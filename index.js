59359deaa91e270521b05c799809e90a9e1ddee5
unknown <apu.islam@g.bracu.ac.bd>
2022-10-25 21:04:03 +0600

    routing update

--- index.js
+++ index.js
@@ -12,6 +12,15 @@ admin.get("/dashboard",(req,res)=>{
     res.send("Welcome to admin dashboard")
 })
 
+// For url routes, case sensitivity will matter
+app.enable("case sensitive routing")
+
+
+// Can be accessed through all methods (GET,PUT, DELEtE ETC)
+app.all("/all",(req,res)=>{
+    res.send("This is a respond for all sort of methods calls")
+})
+
 app.locals.title = "My App"
 app.get("/",(req, res)=>{
     res.send("This is home page");
